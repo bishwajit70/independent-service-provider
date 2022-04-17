2f1c9b99e254b66bc6b5d36dbfc6e5a9475d8239
bishwajit70 <mailtobishwa15@gmail.com>
2022-04-17 09:28:59 +0600

    Github signin method added and working

--- src/component/SocialLogin/SocialLogin.js
+++ src/component/SocialLogin/SocialLogin.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const SocialLogin = () => {
     const navigate = useNavigate()
     const location = useLocation()
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithGithub, gitUser, gitLoading, gitError] = useSignInWithGithub(auth);
 
     let from = location.state?.from?.pathname || "/";
 
-    if (user) {
+    if (googleUser ||gitUser) {
         navigate(from, { replace: true });
     }
 
@@ -20,7 +21,7 @@ const SocialLogin = () => {
                 <button onClick={() => signInWithGoogle()} className='cursor-pointer duration-700 bg-blue-500 hover:bg-blue-600 text-white font-semibold uppercase p-2 rounded'>Continue With Google</button>
             </div>
             <div className='grid my-2 text-left md:w-4/5 mx-auto'>
-                <button className='cursor-pointer duration-700 bg-blue-500 hover:bg-blue-600 text-white font-semibold uppercase p-2 rounded'>Continue With GitHub</button>
+                <button onClick={() => signInWithGithub()} className='cursor-pointer duration-700 bg-blue-500 hover:bg-blue-600 text-white font-semibold uppercase p-2 rounded'>Continue With GitHub</button>
             </div>
         </div >
     );
